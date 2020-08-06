dc9c5f38c02378f40ec318a1707a283b0ab68c36
mukesh <mukeshmahajan501@gmail.com>
2020-08-06 07:46:01 +0530

    Added: printBoard() function to printing board

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,8 +4,9 @@ public class TicTacToe {
 
 	// declare array
 	static String[] board = new String[9];
+	// declare variable
 	static String turn;
-	
+
 	// function to reset the board and set square position 1 to 9
 	static void resetBoard() {
 		for (int a = 0; a < 9; a++) {
@@ -27,19 +28,30 @@ public class TicTacToe {
 	// function to decide who plays first
 	public static void getPlayerFirst() {
 		if (toss == 1) {
-			turn="X";
-			System.out.println("player 1 won the toss!!!"+"choose letter: "+turn);
+			turn = "X";
+			System.out.println("player 1 won the toss!!!" + "choose letter: " + turn);
 
 		} else {
 			System.out.println("player 2  won the toss!!!");
 
 		}
 	}
+
+	// function to print the board
+	static void printBoard() {
+		System.out.println("/---|---|---\\");
+		System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
+		System.out.println("/---|---|---\\");
+	}
+
 	public static void main(String[] args) {
 		resetBoard();
 		getPlayerFirst();
+		printBoard();
 	}
-
-
 
 }
